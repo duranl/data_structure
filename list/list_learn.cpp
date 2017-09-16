4261a7b880f112c6b79b464371311a4af9cbd00d
Duran L <dongyangluo@gmail.com>
2017-09-16 22:47:28 +0800

    Edit the name of typename

--- list/list_learn.cpp
+++ list/list_learn.cpp
@@ -10,21 +10,21 @@
 
 using namespace std;
 
-template<typename container, typename object>
-void change(container &c, const object &newValue)
+template <typename _Container, typename _Object>
+void change(_Container &c, const _Object &newValue)
 {
-	typename container::iterator iter = c.begin();
+	typename _Container::iterator iter = c.begin();
 	while (iter != c.end())
 		*iter++ = newValue;
 }
 
-template <typename container>
-void printCollection(const container &c, ostream &out = cout)
+template <typename _Container>
+void printCollection(const _Container &c, ostream &out = cout)
 {
 	if (c.empty())
 		cout << "(empty)";
 	else {
-		typename container::const_iterator iter = c.begin();
+		typename _Container::const_iterator iter = c.begin();
 		out << "[ " << *iter++; // Print first item
 
 		while (iter != c.end())
